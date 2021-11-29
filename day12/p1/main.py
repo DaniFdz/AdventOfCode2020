0196555138bc602e81110678efe66a9cbdcc9d93
DaniFdz <100451063@alumnos.uc3m.es>
2021-11-29 11:09:50 +0100

    day12 working

--- day12/p1/main.py
+++ day12/p1/main.py
@@ -1,10 +1,39 @@
+from dataclasses import dataclass
+import numpy as np
+
+@dataclass
+class Move:
+    direction: str
+    value: int
+
 class Solution:
     def __init__(self, file:str="input.txt"):
         with open(file, 'r') as f:
-            ...
+            self.input = [Move(x[0], int(x[1:])) for x in f.read().split("\n")[:-1]]
 
     def solve(self):
-        ...
+        x = y = 0
+        direction = 90
+        for move in self.input:
+            if move.direction ==  "F":
+                x += int(move.value * np.sin(direction*np.pi/180))
+                y += int(move.value * np.cos(direction*np.pi/180))
+            elif move.direction ==  "L":
+                direction -= move.value
+                direction %= 360
+            elif move.direction ==  "R":
+                direction += move.value
+                direction %= 360
+            elif move.direction ==  "N":
+                y += move.value
+            elif move.direction ==  "E":
+                x += move.value
+            elif move.direction ==  "S":
+                y -= move.value
+            elif move.direction ==  "W":
+                x -= move.value
+
+        return abs(x) + abs(y)
 
 if __name__=='__main__':
 	print(Solution("day12/p1/input.txt").solve())
--- day12/p2/main.py
+++ day12/p2/main.py
@@ -1,10 +1,45 @@
+from dataclasses import dataclass
+import math
+
+@dataclass
+class Move:
+    direction: str
+    value: int
+
 class Solution:
     def __init__(self, file:str="input.txt"):
         with open(file, 'r') as f:
-            ...
+            self.input = [Move(x[0], int(x[1:])) for x in f.read().split("\n")[:-1]]
 
     def solve(self):
-        ...
+        def rotate(direction, degrees):
+            newDir = direction.copy()
+            senTheta = math.sin(degrees)
+            cosTheta = math.cos(degrees)
+            newDir[0] = int(round(cosTheta*direction[0]-senTheta*direction[1]))
+            newDir[1] = int(round(senTheta*direction[0]+cosTheta*direction[1]))
+            return newDir
+
+        pos = [0, 0]
+        direction = [10, 1]
+        for move in self.input:
+            if move.direction ==  "F":
+                pos[0] += direction[0] * move.value
+                pos[1] += direction[1] * move.value
+            elif move.direction ==  "L":
+                direction = rotate(direction, math.radians(move.value))
+            elif move.direction ==  "R":
+                direction = rotate(direction, math.radians(-move.value))
+            elif move.direction ==  "N":
+                direction[1] += move.value
+            elif move.direction ==  "E":
+                direction[0] += move.value
+            elif move.direction ==  "S":
+                direction[1] -= move.value
+            elif move.direction ==  "W":
+                direction[0] -= move.value
+
+        return abs(pos[0]) + abs(pos[1])
 
 if __name__=='__main__':
 	print(Solution("day12/p2/input.txt").solve())
